a8601d34026ecfc128494bb8111c27b950058d6d
DariushKhaksari <khaksari.dariush@gmail.com>
2022-10-27 14:33:36 +0330

    Change in code

--- rock,paper,scissors.py
+++ rock,paper,scissors.py
@@ -1,4 +1,4 @@
-from random import*
+from random import *
 print(""" 
 Welcome to my game, Be careful maybe this be the last time you are life... \U0001f923 \U0001f923 \U0001f923 \U0001f923 
 they are your moves.
@@ -7,34 +7,34 @@ they are your moves.
  Scissor \U0001F60E
  ---------------""")
 
-randomMove=randint(1, 3)
-if randomMove==1:
-    pcMove="Rock"
-elif randomMove==2:
-    pcMove="Paper"
-elif randomMove==3:
-    pcMove="Scissor"
+randomMove = randint(1, 3)
+if randomMove == 1:
+    pcMove = "rock"
+elif randomMove == 2:
+    pcMove = "paper"
+elif randomMove == 3:
+    pcMove = "scissor"
 
-player_1=input("player_1, make your move: ")
+player_1 = input("player_1, make your move: ").lower()
 print(f"move of player_2 is {pcMove}")
-player_2=pcMove
+player_2 = pcMove
 
-if player_1==player_2:
+if player_1 == player_2:
     print("thats a tie")
-elif player_1=="Rock":
-    if player_2=="Paper":
+elif player_1 == "rock":
+    if player_2 == "paper":
         print("player_2 wins")
-    elif player_2=="Scissor":
+    elif player_2 == "scissor":
         print("player_1 wins")
-elif player_1=="Paper":
-    if player_2=="Rock":
+elif player_1 == "paper":
+    if player_2 == "rock":
         print("player_1 wins")
-    elif player_2=="Scissor":
+    elif player_2 == "scissor":
         print("player_2 wins")
-elif player_1=="Scissor":
-    if player_2=="Paper":
+elif player_1 == "scissor":
+    if player_2 == "paper":
         print("player_1 wins")
-    elif player_2=="Rock":
+    elif player_2 == "rock":
         print("player_2 wins")
 else:
     print("somthing is wrong")
